0455f026255cad66a5c8f34968e5adb7029b8d12
William May <wjrm500@gmail.com>
2022-03-25 07:15:14 +0800

    Date string conversion logic

---
+++ date_conversion.py
@@ -0,0 +1,8 @@
+from time import strftime, strptime
+
+def convert_date(match_date: str, season: str) -> str:
+    match_time_sans_year = strptime(match_date, '%b %d')
+    month = match_time_sans_year.tm_mon
+    [start_year, end_year] = list(map(int, season.split('-')))
+    year = start_year if month >= 8 else end_year + 2000
+    return strftime(f'{year}-%m-%d', match_time_sans_year)
