8696e4f39d33b2c6b3a5d25b8459ee68c66129db
William May <wjrm500@gmail.com>
2022-04-22 01:31:49 +0100

    Completing script for getting fixtures

--- get_fixtures.py
+++ get_fixtures.py
@@ -1,4 +1,7 @@
+from dotenv import load_dotenv
 import json
+import mysql.connector
+import os
 import requests
 
 ### Get team ID - name mapping
@@ -20,6 +23,20 @@ for fixture in fixtures:
         fixture_inserts.append(f'({fixture["event"]}, "{teams[h_team_id]}", "{teams[a_team_id]}", "H")')
         fixture_inserts.append(f'({fixture["event"]}, "{teams[a_team_id]}", "{teams[h_team_id]}", "A")')
 fixture_insert_string = ', '.join(fixture_inserts)
+
+### Insert into database
 table = 'fantrax.fixtures'
+drop_table_sql = f'DROP TABLE IF EXISTS {table}'
+create_table_sql = f'CREATE TABLE {table} (gameweek INT, team CHAR(3), opp CHAR(3), `where` CHAR(1), INDEX (gameweek), INDEX (team), INDEX (opp), INDEX (`where`))'
 fixture_insert_sql = f'INSERT INTO {table} VALUES {fixture_insert_string}'
-a = 1
+load_dotenv()
+cnx = mysql.connector.connect(
+    user =  os.environ.get('DB_USER'),
+    password = os.environ.get('DB_PASSWORD'),
+    host = os.environ.get('DB_HOST'),
+    port = os.environ.get('DB_PORT')
+)
+cursor = cnx.cursor()
+for statement in [drop_table_sql, create_table_sql, fixture_insert_sql]:
+    cursor.execute(statement)
+    cnx.commit()
